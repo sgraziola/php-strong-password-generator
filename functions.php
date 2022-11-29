8c98b0b40e403473652d45f96b9fbddccf64ed23
sgraziola <simo.fdl@gmail.com>
2022-11-29 18:01:21 +0100

    PHP PswGen: add logic to function.php

--- functions.php
+++ functions.php
@@ -1,4 +1,31 @@
 <?php
+$letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+$numbers = '0123456789';
+$symbols = '@#!$%&?';
+
+
+$characters = '';
+
+if (isset($_POST['letters'])) {
+    // var_dump('vuole lettere');
+    $characters .= $letters;
+}
+if (isset($_POST['numbers'])) {
+    // var_dump('vuole numeri');
+    $characters .= $numbers;
+}
+if (isset($_POST['symbols'])) {
+    //var_dump('vuole simboli');
+    $characters .= $symbols;
+}
+//var_dump($_POST['length']);
+//var_dump($characters);
+//var_dump($_POST['rep']);
+if (isset($_POST['rep'])) {
+    //var_dump($_POST['rep']);
+    $randomPsw = getRandomPassword($_POST['length'], $characters, $_POST['rep']);
+}
+
 function getRandomPassword($pswLength, $permittedChars, $reps)
 {
     $password = '';
