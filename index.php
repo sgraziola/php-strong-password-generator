40d5c40f73232664c920912a568db9cacf855f14
sgraziola <simo.fdl@gmail.com>
2022-11-29 14:18:28 +0100

    PHP PswGen: Initial Commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+/* 
+Milestone 1
+Creare un form che invii in GET la lunghezza della password. Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente. Scriviamo tutto (logica e layout) in un unico file index.php 
+*/
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password Generator</title>
+
+    <!-- Bootstrap CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+
+<body>
+    <form action="index.php" method="get">
+        <label for="length">Inserisci la lunghezza della password da generare:</label>
+        <input type="number" name="length" id="length">
+        <div>
+            <button type="submit">Invia</button>
+            <button type="reset">Annulla</button>
+        </div>
+    </form>
+
+</body>
+
+</html>
